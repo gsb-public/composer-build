9ef3921a1a326db006bee0d992b08dcbd7298200
Greg Mercer <gmercer@stanford.edu>
2019-04-02 08:47:01 -0700

    Update doupdate.sh

--- scripts/doupdate.sh
+++ scripts/doupdate.sh
@@ -2,15 +2,19 @@
 
 PROJECT_DIR="."
 DISTRO="gsb-public"
-REMOTE_URL="<url to stage site>"
+REMOTE_URL="<dev/dev2/stage/stage2 server url>"
 SITES_DIR=$PROJECT_DIR/sites/default
 TMP_DIR="temp"
 
 sudo ln -s $SITES_DIR $PROJECT_DIR/sites/gsb
 
+echo "Disable Memcache, Acquia and Shield"
+lando drush dis -y memcache_admin acquia_purge acquia_agent shield
+
 echo "Set site variables"
-lando drush upwd --password=admin admin
+lando drush vset file_private_path $TMP_DIR
 lando drush vset file_temporary_path $TMP_DIR
+lando drush upwd --password=admin admin
 lando drush vset cache 0
 lando drush vset preprocess_css 0
 lando drush vset preprocess_js 0
@@ -22,23 +26,13 @@ lando drush updb -y
 echo "Revert features"
 lando drush fra -y
 
-echo "Clearing cache"
-lando drush cc all
-
-echo "Disable Memcache, Acquia and Shield"
-lando drush dis -y memcache_admin acquia_purge acquia_agent shield
-
 echo "Enable Stage File Proxy and Devel"
 lando drush en -y devel stage_file_proxy
 lando drush vset stage_file_proxy_origin $REMOTE_URL
 
 echo "Enable views development setup."
 lando drush vd
+
+echo "Clearing cache"
 lando drush cc all
 
-# Update site variables
-echo "Updating the site modules and variables"
-lando drush dis -y memcache_admin acquia_purge acquia_agent shield
-lando drush en -y stage_file_proxy
-lando drush vset stage_file_proxy_origin $REMOTE_URL
-lando drush upwd admin --password=admin
